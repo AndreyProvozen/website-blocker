2f5a09f77887c4cc3452c7bf4e7be4a959a525a8
Andrey Provozen <provozena885@gmail.com>
2025-02-18 21:05:21 -0700

    feat: implement time logic on handle on updated

--- src/scripts/background/handleOnUpdated.js
+++ src/scripts/background/handleOnUpdated.js
@@ -1,9 +1,25 @@
 import validateUrl from "../utils/validateUrl.js";
+import { getBlockedSites } from "../utils/storageUtils.js";
 
-const handleOnUpdated = (tabId, { status }, { url }) => {
+const handleOnUpdated = async (tabId, { status }, { url }) => {
   if (status === "complete" && url && validateUrl(url)) {
-    const expirationTime = Date.now() + 3000;
-    chrome.alarms.create(`tab-${tabId}`, { when: expirationTime });
+    const blockedSites = await getBlockedSites();
+    const { hostname } = new URL(url);
+
+    const blockedSite = blockedSites.find(({ link, isWholeDomain }) => {
+      if (isWholeDomain) {
+        const blockedDomain = new URL(link).hostname;
+        return hostname.endsWith(blockedDomain);
+      }
+
+      return url === link;
+    });
+
+    if (blockedSite && blockedSite.timeLeft > 0) {
+      const expirationTime = Date.now() + blockedSite.timeLeft;
+
+      chrome.alarms.create(`tab-${tabId}`, { when: expirationTime });
+    }
   }
 };
 
